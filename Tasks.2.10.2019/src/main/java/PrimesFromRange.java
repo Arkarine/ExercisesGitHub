fb7184e751977edb811ed6227153ebfc94a9c017
Arkarine <karine.arustamyan@gmail.com>
2019-10-21 11:17:11 +0200

    change condition

--- Tasks.2.10.2019/src/main/java/PrimesFromRange.java
+++ Tasks.2.10.2019/src/main/java/PrimesFromRange.java
@@ -7,7 +7,7 @@ public class PrimesFromRange {
         boolean[] boolArr = new boolean[n + 1];
         // we check each element in the range [2, n], if element x is checked -> boolArr[x] = true
         for (int i = 2; i <= n; i++) {
-            if(boolArr[i] == false){
+            if(!boolArr[i]){
                 result.add(i);
                 for (int j = i; j*i <= n; j++) {
                     boolArr[j*i] = true;
