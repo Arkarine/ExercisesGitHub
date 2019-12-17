f8dc83618f0c35f7064686a2379a0f161fde8ea5
Arkarine <karine.arustamyan@gmail.com>
2019-12-17 14:14:15 +0100

    add solution 'Tasks. 11.12.2019'

---
+++ Tasks.11.12.2019/src/main/java/ModulusFinder.java
@@ -0,0 +1,37 @@
+public class ModulusFinder {
+    /*
+    Given n, m where n >= 0, m > 1.
+    Find F(n) % m, where F(n) - n-th Fibonacci, % - operator Modulus
+     */
+
+    public int findFibonacci(int n){  // n >= 0
+
+        if(n <= 1){
+            return n;
+        }
+        int F_first = 0;
+        int F_second = 1;
+        int result = 0;
+
+        for (int i = 2; i <= n; i++) {
+            result = F_first + F_second;
+            F_first = F_second;
+            F_second = result;
+
+        }
+        return result;
+    }
+
+    public int findModulus(int n, int m){   // m >1
+        return findFibonacci(n) % m;
+    }
+
+    //=============================================
+
+    public static void main(String[] args) {
+        ModulusFinder mf = new ModulusFinder();
+        System.out.println(mf.findFibonacci(10));
+        System.out.println(mf.findModulus(10, 10));
+    }
+
+}
